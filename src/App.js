cedfc9bd8e3408f82f8584df7f567e32cb249824
Olga Cybulska <olgacybulska7@wp.pl>
2023-10-24 17:34:00 +0200

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -25,6 +25,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return {...task, done: !task.done};
+      }
+      return task;
+    }));
+  };
+
 
   return (
     <Container>
@@ -34,7 +43,7 @@ function App() {
 
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />}
+        body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />}
         extrHeaderContent={
           <Buttons
             tasks={tasks}
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,13 +1,15 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDone, removeTask}) => (
+const Tasks = ({tasks, hideDone, removeTask, toggleTaskDone}) => (
   <ul className="section__item">
     {tasks.map(task => (
       <li
+      key={task.id}
         className={`list__item 
         ${task.done && hideDone ? "list__item--hidden" : ""}`}
       >
-        <button className="list__button list__button--done">
+        <button className="list__button list__button--done"
+        onClick={() => toggleTaskDone(task.id)}>
           {task.done ? "✔" : ""}
         </button>
         <span className={`${task.done ? "list__task--done" : "list__item--text"}`}>
