6076a71f54a5597dc45de4a55912c4758751e7fe
Olga Cybulska <olgacybulska7@wp.pl>
2023-10-26 16:45:10 +0200

    code formatted

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,20 +1,13 @@
 import "./style.css";
 
-const Section = ({title, body, extrHeaderContent}) => (
-    <section>
+const Section = ({ title, body, extrHeaderContent }) => (
+  <section>
     <div className="section__item section__grid">
       <h2 className="section__header">{title}</h2>
       {extrHeaderContent}
     </div>
-    <div className="section__item">
-    {body}
-    </div>
+    <div className="section__item">{body}</div>
   </section>
 );
-   
 
-
-    
 export default Section;
-
-
