3af03f093dc509d93ac704b4a4918ac19d5c3236
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-17 22:14:47 +0100

    changed name (befor Tasks)

---
+++ src/features/tasks/TasksPage/index.js.js
@@ -0,0 +1,31 @@
+import Form from "../Form";
+import TaskList from "../TaskList";
+import Buttons from "../Buttons";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Container from "../../../common/Container";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  breakpoint: 767,
+};
+
+function TasksPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Header title="Lista zadań" />
+
+        <Section title="Dodaj nowe zadanie" body={<Form />} />
+
+        <Section
+          title="Lista zadań"
+          body={<TaskList />}
+          extrHeaderContent={<Buttons />}
+        />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default TasksPage;
