d3067e88b3a11edc574fee77dec23ff5a323d2f3
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-17 22:08:32 +0100

    AuthorPage initial state

---
+++ src/features/author/AuthorPage.js
@@ -0,0 +1,18 @@
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import Container from "../../common/Container";
+
+export default () => (
+  <Container>
+    <Header title="O autorze" />
+
+    <Section
+      title="Olga Cybulska"
+      body={
+        <>
+          <p>Coś tam coś tam</p>
+        </>
+      }
+    ></Section>
+  </Container>
+);
