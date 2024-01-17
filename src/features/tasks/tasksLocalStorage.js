b375e4d54eebbfd83617dac65a85e8fe893f783a
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-18 00:37:25 +0100

    tasksFromLocalStorage added

--- src/features/tasks/tasksLocalStorage.js
+++ src/features/tasks/tasksLocalStorage.js
@@ -1,7 +1,12 @@
 const localStorageKey = "tasks";
+const tasksFromLocalStorage = localStorage.getItem(localStorageKey);
 
 export const saveTasksInLocalStorage = (tasks) =>
   localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
-export const getTasksFromLocalStorage = () =>
-  JSON.parse(localStorage.getItem(localStorageKey)) || [];
+export const getTasksFromLocalStorage = () => {
+  if (!tasksFromLocalStorage) {
+    return [];
+  }
+  JSON.parse(tasksFromLocalStorage);
+};
