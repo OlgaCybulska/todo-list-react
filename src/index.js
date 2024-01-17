196bea5699012ab097fed0affad3faf485f78a78
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-17 22:07:18 +0100

    store changed place

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { App } from "./App";
 import store from "./store";
+import { App } from "./App";
 import { Provider } from "react-redux";
 import { GlobalStyle } from "./GlobalStyle";
 
