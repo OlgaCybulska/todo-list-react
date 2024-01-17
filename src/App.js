6fe201b5c47c66b94530778fc64c80434c918cde
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-17 22:06:58 +0100

    using react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
-import Tasks from "./features/tasks/Tasks";
+import {
+  HashRouter,
+  Link,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom/cjs/react-router-dom.min";
+import TasksPage from "./features/tasks/TasksPage/index.js";
+import TaskPage from "./features/tasks/TaskPage/index.js";
+import AuthorPage from "./features/author/AuthorPage";
 
-export const App = () => <Tasks />;
+export const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/autor">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/zadania" />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
