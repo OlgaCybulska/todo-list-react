6ef0846a70d65e2e240dedf6ee525aadc37a7db1
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-21 17:52:37 +0100

    initial state

---
+++ src/features/tasks/TasksPage/Search/index.js
@@ -0,0 +1,35 @@
+import Input from "../../Input";
+import {
+  useHistory,
+  useLocation,
+} from "react-router-dom/cjs/react-router-dom.min";
+import { Wrapper } from "./styled";
+import searchQueryParamName from "../../TaskList/searchQueryParamName";
+
+export default () => {
+  const location = useLocation();
+  const history = useHistory();
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+
+  const onInputChange = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (target.value.trim() === "") {
+      searchParams.delete(searchQueryParamName);
+    } else {
+      searchParams.set(searchQueryParamName, target.value);
+    }
+
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <Wrapper>
+      <Input
+        placeholder="Filtruj zadania"
+        value={query || ""}
+        onChange={onInputChange}
+      ></Input>
+    </Wrapper>
+  );
+};
