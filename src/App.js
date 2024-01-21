94e1a67457f1c9a6a472106631aa2ee4aedea3b0
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-21 22:00:09 +0100

    added function with path to name

--- src/App.js
+++ src/App.js
@@ -8,30 +8,31 @@ import TasksPage from "./features/tasks/TasksPage/index.js";
 import TaskPage from "./features/tasks/TaskPage/index.js";
 import AuthorPage from "./features/author/AuthorPage";
 import { StyledNavLink } from "./styled.js";
+import { toAuthor, toTask, toTasks } from "./routes.js";
 
 export const App = () => (
   <HashRouter>
     <nav>
       <ul>
         <li>
-          <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+          <StyledNavLink to={toTasks()}>Zadania</StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/autor">O autorze</StyledNavLink>
+          <StyledNavLink to={toAuthor()}>O autorze</StyledNavLink>
         </li>
       </ul>
       <Switch>
-        <Route path="/zadania/:id">
+        <Route path={toTask()}>
           <TaskPage />
         </Route>
-        <Route path="/zadania">
+        <Route path={toTasks()}>
           <TasksPage />
         </Route>
-        <Route path="/autor">
+        <Route path={toAuthor()}>
           <AuthorPage />
         </Route>
         <Route path="/">
-          <Redirect to="/zadania" />
+          <Redirect to={toTasks()} />
         </Route>
       </Switch>
     </nav>
