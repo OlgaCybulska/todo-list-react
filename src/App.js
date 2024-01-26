9abd3a97caee098897119461c4c4a48eef4721eb
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-26 20:15:11 +0100

    Navigation added

--- src/App.js
+++ src/App.js
@@ -9,32 +9,24 @@ import TaskPage from "./features/tasks/TaskPage/index.js";
 import AuthorPage from "./features/author/AuthorPage";
 import { StyledNavLink } from "./styled.js";
 import { toAuthor, toTask, toTasks } from "./routes.js";
+import Navigation from "./Navigation/index.js";
 
 export const App = () => (
   <HashRouter>
-    <nav>
-      <ul>
-        <li>
-          <StyledNavLink to={toTasks()}>Zadania</StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to={toAuthor()}>O autorze</StyledNavLink>
-        </li>
-      </ul>
-      <Switch>
-        <Route path={toTask()}>
-          <TaskPage />
-        </Route>
-        <Route path={toTasks()}>
-          <TasksPage />
-        </Route>
-        <Route path={toAuthor()}>
-          <AuthorPage />
-        </Route>
-        <Route path="/">
-          <Redirect to={toTasks()} />
-        </Route>
-      </Switch>
-    </nav>
+    <Navigation />
+    <Switch>
+      <Route path={toTask()}>
+        <TaskPage />
+      </Route>
+      <Route path={toTasks()}>
+        <TasksPage />
+      </Route>
+      <Route path={toAuthor()}>
+        <AuthorPage />
+      </Route>
+      <Route path="/">
+        <Redirect to={toTasks()} />
+      </Route>
+    </Switch>
   </HashRouter>
 );
