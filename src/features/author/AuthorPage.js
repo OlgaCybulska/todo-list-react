97537c8f2ce032237c0a34a14c8b8bb8a555bea5
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-26 20:18:05 +0100

    text added

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -10,9 +10,23 @@ export default () => (
       title="Olga Cybulska"
       body={
         <>
-          <p>Coś tam coś tam</p>
+          Jestem absolwentką <strong>Politechniki Gdańskiej</strong> na
+          kierunkach:
+          <ul>
+            <li>Matematyka </li>
+            <li>Mechanika i Budowa Maszyn.</li>
+          </ul>{" "}
+          Od dwóch lat zajmuję się prowadzeniem zajęć indywidualnych z
+          matematyki.
+          <p>Wcześniej pracowałam jako konstruktor.</p> Kilka lat wcześniej
+          chciałam uczyć się programowania, ale zawsze było coś "ważniejszego"
+          😆
+          <p>
+            Po obejrzeniu reklamy na Tik-toku stwierdziłam, że nadszedł
+            odpowieni czas, żeby zrealiwoać swoje marzenie!
+          </p>
         </>
       }
-    ></Section>
+    />
   </Container>
 );
