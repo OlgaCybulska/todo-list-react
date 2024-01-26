87abc8ca214723d82670829fe6b5c7df9f4721e0
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-26 20:17:14 +0100

    StyledNavLink added

--- src/styled.js
+++ src/styled.js
@@ -1,11 +1,1 @@
-import styled from "styled-components";
-import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
-export const StyledNavLink = styled(NavLink)`
-  text-decoration: none;
-
-  &.active {
-    font-weight: bold;
-    color: teal;
-  }
-`;
