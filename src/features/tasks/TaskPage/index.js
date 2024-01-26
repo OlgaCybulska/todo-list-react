60cdc43bbd9f6d7cbe751169671c87ee472fd235
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-26 20:21:40 +0100

    empty task condition added

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -15,9 +15,11 @@ function TaskPage() {
       <Section
         title={task ? task.content : "Nie znaleziono zadania 😪 "}
         body={
-          <>
-            <strong>Ukończono: </strong> {task.done ? "Tak" : "Nie"}
-          </>
+          !!task && (
+            <>
+              <strong>Ukończono: </strong> {task.done ? "Tak" : "Nie"}
+            </>
+          )
         }
       />
     </Container>
