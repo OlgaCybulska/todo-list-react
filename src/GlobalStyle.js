3895b490a46d4cb16e67331405ae92c92f56bdef
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-26 20:15:57 +0100

    #root  max-width removed

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,13 +12,10 @@ html {
 }
 
 #root {
-  max-width: 1000px;
   width: 100%;
   margin-top: 5px;
-  padding: 40px;
   background-color: hsl(0, 0%, 95%);
   margin: auto;
-
 }
 
 @media (max-width: 767px) {
